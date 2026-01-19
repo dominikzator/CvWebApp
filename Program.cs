61e22c0383f219a30ab5674e6b00eef9838c4ad2
Zatii <dominikzator@gmail.com>
2026-01-20 00:08:08 +0100

    added ClaimsPrincipal initialization

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
@@ -14,6 +15,26 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-ID"))
+    {
+        var azureAppServicePrincipalIdHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"].FirstOrDefault();
+        var azureAppServicePrincipalNameHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"].FirstOrDefault();
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, azureAppServicePrincipalIdHeader),
+            new Claim(ClaimTypes.Name, azureAppServicePrincipalNameHeader ?? azureAppServicePrincipalIdHeader)
+        };
+
+        var identity = new ClaimsIdentity(claims, "AzureAppService");
+        context.User = new ClaimsPrincipal(identity);
+    }
+
+    await next();
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

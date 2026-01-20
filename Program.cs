7a721670bc8c797bdce7271842c13bfffe398add
Zatii <dominikzator@gmail.com>
2026-01-20 23:09:49 +0100

    Removed bug on production

--- Program.cs
+++ Program.cs
@@ -11,14 +11,6 @@ builder.Services.AddDbContext<MainDBContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.Use(async (context, next) =>
 {
     if (context.Request.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-ID"))
